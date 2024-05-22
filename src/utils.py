b33924aa43ceef7a939f40bd33ccdcf5f2b840e9
Tong Wu <39028016+Anthonysdu@users.noreply.github.com>
2024-05-22 15:55:50 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+import re
 
 FAILED = "VERIFICATION FAILED"
 SUCCESS = "VERIFICATION SUCCESSFUL"
@@ -56,3 +57,41 @@ def run_command(command):
             return f"Command execution failed with error: {combined_output}"
     except Exception as e:
         return f"An error occurred: {str(e)}"
+
+
+def get_loops(tool, files):
+    command = tool + " " + files + " " + "--show-loops"
+    pattern = r"(Loop|goto-loop Loop)\s+(?P<loop_name>[^:]+):\s+file\s+(?P<file_path>.*?)\s+line\s+(?P<line_number>\d+)\s*(?:column\s+(?P<column_number>\d+))?\s+function\s+(?P<function_name>\S+)"
+    regex = re.compile(pattern)
+    loops = {}
+    matches = regex.findall(run_command(command))
+    for match in matches:
+        loop_name = match[2] if match[0] == "goto-loop" else match[1]
+        function_name = match[5]
+        loops[function_name] = loop_name
+    return loops
+
+
+def increment_bound(unwindset, loopid, depth):
+    if loopid in unwindset:
+        unwindset[loopid] += depth
+
+
+def decrement_bound(unwindset, loopid, depth):
+    if loopid in unwindset:
+        if unwindset[loopid] - depth > 0:
+            unwindset[loopid] -= depth
+        else:
+            unwindset[loopid] = 1
+
+
+def set_bound(unwindset, loopid, depth):
+    if loopid in unwindset:
+        unwindset[loopid] = depth
+
+
+# init loops unrolling depths
+def init_bounds(unwindset, loops, depth):
+    unwindset.clear()
+    for loop in loops.values():
+        unwindset[loop] = depth
